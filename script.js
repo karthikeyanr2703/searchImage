a16d0b5f153cda168ec82f288e54617d52f1fc9a
KARTHIKEYAN R <karthikeyanr2703@gmail.com>
2024-01-29 16:06:52 +0530

    final files added

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 let search = document.querySelector(".searchBox input");
-let images = document.querySelector(".imageBox");
+let images = document.querySelectorAll(".imageBox");
+search.addEventListener('keyup', function(e) {
+   
+    if(e.key =="Enter"){
+        let searchValue = search.value
+        let value = searchValue.toLowerCase();
+        images.forEach(image => {
+           
+            if(value == image.dataset.name){
+                image.style.display = 'block';
+            }
+            else{
+                image.style.display = 'none'
+            }
+            
+        });
+    }
+  });
+  search.addEventListener("keyup", function(e) {
+    if(search.value != ""){
+        return 
+    }
+    images.forEach(image => {
+        image.style.display ="block"
+    });
+
+  })
